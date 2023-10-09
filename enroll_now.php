beecb8f91a233ae92b1f85a002bbd263c551325f
kg16zxc <keithgals16@gmail.com>
2023-10-10 02:19:31 +0800

    first commit

---
+++ enroll_now.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+
+    <title>Simple Navigation Bar</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <nav>
+            <li class="cdonhs_logo"><img src="CDONHS_LOGO.jpg" alt="Cdonhs_logo" height="80" width="80"></li> 
+            <li><a href="index.php">Home</a></li>
+        </ul>
+    </nav>
+
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+
+    <title>Simple Navigation Bar</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <nav>
+            <li class="cdonhs_logo"><img src="CDONHS_LOGO.jpg" alt="Cdonhs_logo" height="80" width="80"></li>     
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Login</a></li>
+            <li><a href="enroll_now.php">Enroll Now</a></li>
+            
+        </ul>
+    </nav>
+
+    <div class="cdonhs_home">
+        <img src="IMG_Campus.jpg" alt="Cdonhs_logo"> 
+        <div class="text">Cagayan de Oro Senior High School</div>
+    </div>
+        
+            
+
+</body>
+</html>
